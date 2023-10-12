0e8d8c388436b396503fac329bc240c88fe6e569
jackbomb44 <rimmelasghar3@gmail.com>
2023-10-12 20:08:09 +0500

    Added: Some Parameters to Support CheckOut

--- src/main/java/org/example/Params.java
+++ src/main/java/org/example/Params.java
@@ -3,4 +3,7 @@ package org.example;
 public class Params {
     public String userName = "standard_user";
     public  String userPassword = "secret_sauce";
+    public String checkoutUserFirstName = "Rimmel";
+    public  String checkoutUserLastName = "Asghar";
+    public  String checkoutPostalCode = "45372";
 }
