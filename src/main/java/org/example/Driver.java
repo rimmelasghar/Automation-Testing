bb4ee988461559c56ac13da36c293215bec9f7ea
jackbomb44 <rimmelasghar3@gmail.com>
2023-10-12 19:30:02 +0500

    Fixed: Driver Location

--- src/main/java/org/example/Driver.java
+++ src/main/java/org/example/Driver.java
@@ -8,19 +8,19 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Driver {
     public static WebDriver getDriver(String browserName) {
         WebDriver driver = null;
-
+        String projectPath = System.getProperty("user.dir");
         switch (browserName.toLowerCase()) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", "C:/Users/840 G3/Desktop/Folders/SeleniumTesting/TestingSQA/src/main/resources/chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", projectPath + "/src/main/resources/chromedriver.exe");
                 driver = new ChromeDriver();
                 break;
             case "firefox":
-                System.setProperty("webdriver.gecko.driver", "C:/Users/840 G3/Desktop/Folders/SeleniumTesting/TestingSQA/src/main/resources/geckodriver.exe");
+                System.setProperty("webdriver.gecko.driver", projectPath + "/src/main/resources/geckodriver.exe");
                 driver = new FirefoxDriver();
                 break;
             default:
                 // default Driver is MicroSoft Edge
-                System.setProperty("webdriver.edge.driver", "C:/Users/840 G3/Desktop/Folders/SeleniumTesting/TestingSQA/src/main/resources/msedgedriver.exe");
+                System.setProperty("webdriver.edge.driver", projectPath + "/src/main/resources/msedgedriver.exe");
                 driver = new EdgeDriver();
         }
         return driver;
