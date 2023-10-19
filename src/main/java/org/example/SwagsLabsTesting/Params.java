f7f68842ef0bd48a18d8f451aceccf5b7eb30ab4
jackbomb44 <rimmelasghar3@gmail.com>
2023-10-19 15:31:12 +0500

    Modified: Changes in Params

--- src/main/java/org/example/SwagsLabsTesting/Params.java
+++ src/main/java/org/example/SwagsLabsTesting/Params.java
@@ -2,9 +2,12 @@ package org.example.SwagsLabsTesting;
 
 public class Params {
 //    Parameters for SwagsLab Website.
-    public String userName = "standard_user";
-    public  String userPassword = "secret_sauce";
-    public String checkoutUserFirstName = "Rimmel";
-    public  String checkoutUserLastName = "Asghar";
-    public  String checkoutPostalCode = "45372";
+
+    public static int waitTime = 5;
+    public static String userName = "standard_user";
+    public static String userPassword = "secret_sauce";
+    public static String checkoutUserFirstName = "Rimmel";
+    public static String checkoutUserLastName = "Asghar";
+    public static String checkoutPostalCode = "45372";
+    public static String urlPath = "https://www.saucedemo.com/";
 }
